51bd38bc9b5bb02a6275c5d948428ef2a4d29946
habfem <habeebmustapha96@gmail.com>
2023-04-21 08:23:55 -0500

    production

--- backend/app.js
+++ backend/app.js
@@ -16,8 +16,18 @@ app.use(cors()); //for domain you want it to be in
 //routes
 app.use("/api/v1", transctionsRoute)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...');
+  });
+}
+
 const PORT = 5000 || process.env.PORT
 
 app.listen(PORT, () => {
-  console.log(`Server Running on port: ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port: ${PORT}`)
 })
